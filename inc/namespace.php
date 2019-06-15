ebd968c7954920c7751bc58980932b014a18f63a
Peter Wilson <git-commit-by@peterwilson.cc>
2019-06-15 14:37:15 +1000

    Ensure the table prefix is valid.

--- inc/namespace.php
+++ inc/namespace.php
@@ -12,3 +12,31 @@ const CACHE_GROUP = 'rapid-cron';
 function bootstrap() {
 	wp_cache_add_global_groups( CACHE_GROUP );
 }
+
+/**
+ * Tidy up the defined prefix to ensure it's valid.
+ *
+ * @param bool $die Die if the prefix is invalid. Default true.
+ * @return string PRFIX constant modified to be SQL table name safe.
+ */
+function get_table_prefix( $die = true ) {
+	$prefix = preg_replace( '|[^a-z0-9_]|i', '', PREFIX );
+	if ( $prefix && substr( $prefix, -1 ) === '_' ) {
+		return $prefix;
+	} elseif ( $prefix ) {
+		return "{$prefix}_";
+	}
+
+	// Invalid prefix.
+	if ( $die ) {
+		wp_die(
+			__( 'Rapid Cron prefix is invalid, a valid MYSQL table name can not be generated.', 'rapid_cron' ),
+			__( 'An error has occurred.', 'rapid-cron' )
+		);
+	}
+
+	return new \WP_Error(
+		'rapid_cron_invalid_prefix',
+		__( 'Rapid Cron prefix is invalid, a valid MYSQL table name can not be generated.', 'rapid_cron' )
+	);
+}
