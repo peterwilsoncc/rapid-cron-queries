a8c1bec50b3ac98db96b1f3e43d56cd81164c271
Peter Wilson <git-commit-by@peterwilson.cc>
2019-06-15 14:39:03 +1000

    Determine if the plugin is installed.

--- inc/namespace.php
+++ inc/namespace.php
@@ -40,3 +40,29 @@ function get_table_prefix( $die = true ) {
 		__( 'Rapid Cron prefix is invalid, a valid MYSQL table name can not be generated.', 'rapid_cron' )
 	);
 }
+
+/**
+ * Is the plugin installed?
+ *
+ * Used during the plugin's bootstrapping process to create the table.
+ *
+ * Source: Cavalcade by Human Made.
+ *
+ * @return boolean
+ */
+function is_installed() {
+	global $wpdb;
+	if ( wp_cache_get( 'installed', CACHE_GROUP ) ) {
+		return true;
+	}
+
+	$table_prefix = get_table_prefix();
+	$installed = ( count( $wpdb->get_col( "SHOW TABLES LIKE '{$wpdb->base_prefix}{$table_prefix}%'" ) ) === 2 );
+
+	if ( $installed ) {
+		// Don't check again :)
+		wp_cache_set( 'installed', $installed, CACHE_GROUP );
+	}
+
+	return $installed;
+}
