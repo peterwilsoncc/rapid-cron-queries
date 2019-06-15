ff2a95f0f6a4ca57e063554704d6afd6ed07aea7
Peter Wilson <git-commit-by@peterwilson.cc>
2019-06-15 14:01:05 +1000

    Registe global cache groups.

--- inc/namespace.php
+++ inc/namespace.php
@@ -4,9 +4,11 @@ namespace PWCC\RapidCronQueries;
 const PREFIX     = 'rapid_cron_';
 const DB_VERSION = 1;
 const MYSQL_DATE_FORMAT = 'Y-m-d H:i:s';
+const CACHE_GROUP = 'rapid-cron';
 
 /**
  * Kick it off.
  */
 function bootstrap() {
+	wp_cache_add_global_groups( CACHE_GROUP );
 }
