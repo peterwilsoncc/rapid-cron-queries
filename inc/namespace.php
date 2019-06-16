5878ba5d65f7fce4970434b8ba274d9f64513df6
Peter Wilson <git-commit-by@peterwilson.cc>
2019-06-16 11:20:25 +1000

    Don’t allow for invalid DB Prefix.

--- inc/namespace.php
+++ inc/namespace.php
@@ -5,35 +5,11 @@ namespace PWCC\RapidCronQueries;
  * Kick it off.
  */
 function bootstrap() {
+	if ( preg_replace( '|[^a-z0-9_]|i', '', DB_PREFIX ) !== DB_PREFIX ) {
+		// Invalid Database Prefix, bail.
+		return;
+	}
 	wp_cache_add_global_groups( CACHE_GROUP );
-}
-
-/**
- * Tidy up the defined prefix to ensure it's valid.
- *
- * @param bool $die Die if the prefix is invalid. Default true.
- * @return string PRFIX constant modified to be SQL table name safe.
- */
-function get_table_prefix( $die = true ) {
-	$prefix = preg_replace( '|[^a-z0-9_]|i', '', PREFIX );
-	if ( $prefix && substr( $prefix, -1 ) === '_' ) {
-		return $prefix;
-	} elseif ( $prefix ) {
-		return "{$prefix}_";
-	}
-
-	// Invalid prefix.
-	if ( $die ) {
-		wp_die(
-			__( 'Rapid Cron prefix is invalid, a valid MYSQL table name can not be generated.', 'rapid_cron' ),
-			__( 'An error has occurred.', 'rapid-cron' )
-		);
-	}
-
-	return new \WP_Error(
-		'rapid_cron_invalid_prefix',
-		__( 'Rapid Cron prefix is invalid, a valid MYSQL table name can not be generated.', 'rapid_cron' )
-	);
 }
 
 /**
@@ -51,11 +27,10 @@ function is_installed() {
 		return true;
 	}
 
-	$table_prefix = get_table_prefix();
-	$installed = ( count( $wpdb->get_col( "SHOW TABLES LIKE '{$wpdb->base_prefix}{$table_prefix}%'" ) ) === 2 );
+	$installed = ( count( $wpdb->get_col( "SHOW TABLES LIKE '{$wpdb->base_prefix}{DB_PREFIX}%'" ) ) === 2 );
 
 	if ( $installed ) {
-		// Don't check again :)
+		// Don't check again.
 		wp_cache_set( 'installed', $installed, CACHE_GROUP );
 	}
 
