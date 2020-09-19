96fd974585f41326e5e87eb45c3125704f3c17c9
Peter Wilson <wilson@peterwilson.cc>
2020-09-20 09:28:48 +1000

    Things and stuff

--- inc/namespace.php
+++ inc/namespace.php
@@ -1,6 +1,10 @@
 <?php
 namespace PWCC\WP_Cron_Cavalcade;
 
+const CACHE_GROUP = 'PWCC_RAPID_CRON_QUERIES';
+
+
+
 /**
  * Initialize the plugin.
  *
@@ -25,12 +29,80 @@ function bootstrap() {
 	}
 
 	/*
+	 * Cavalcade disables persistent caching due to the runner's control of
+	 * rescheduling. As this plugin is intended to run using the standard WP-CLI
+	 * command and to reschedule events, persistent caching is available.
+	 *
+	 * Cache values for
+	 * - pre_get_scheduled_event
+	 *
+	 * Clear event cache for
+	 * - pre_reschedule_event
+	 * - pre_unschedule_event
+	 *
+	 * Clear total cache for
+	 * - pre_clear_scheduled_hook
+	 * - pre_unschedule_hook
+	 *
+	 * Clear `null` cache for
+	 * - pre_schedule_event
+	 */
+	add_filter( 'pre_get_scheduled_event', __NAMESPACE__ . '\\get_scheduled_event_get_from_cache', 9, 4 );
+	add_filter( 'pre_get_scheduled_event', __NAMESPACE__ . '\\get_scheduled_event_add_to_cache', 11, 4 );
+
+	add_filter( 'pre_reschedule_event', __NAMESPACE__ . '\\clear_event_cache_by_obj', 11, 2 );
+	add_filter( 'pre_unschedule_event', __NAMESPACE__ . '\\clear_event_cache', 11, 4 );
+
+	/*
 	 * The Cavalcade plugin uses a different rescheduling algorithm to WordPress
 	 * Core. This overrides the Cavalcade changes to use the WordPress default.
 	 *
 	 * This runs early to ensure Cavalcade defers to the override.
 	 */
 	add_filter( 'pre_reschedule_event', __NAMESPACE__ . '\\pre_reschedule_event', 9, 2 );
+}
+
+function event_cache_key( $hook, $args, $timestamp ) {
+	return sha1( serialize( [ $hook, $args, $timestamp ] ) );
+}
+
+function get_scheduled_event_get_from_cache( $pre, $hook, $args, $timestamp ) {
+	if ( $pre !== null ) {
+		// Allow other preflight plugins to do their thing.
+		return $pre;
+	}
+
+	$found = false;
+	$cache_key = event_cache_key( $hook, $args, $timestamp );
+	$value = wp_cache_get( $cache_key, CACHE_GROUP, false, $found );
+	if ( ! $found ) {
+		return $pre;
+	}
+	return $value;
+}
+
+function get_scheduled_event_add_to_cache( $pre, $hook, $args, $timestamp ) {
+	if ( $pre === null ) {
+		// Nothing to cache.
+		return $pre;
+	}
+
+	$cache_key = event_cache_key( $hook, $args, $timestamp );
+	wp_cache_set( $cache_key, $pre, CACHE_GROUP );
+	return $pre;
+}
+
+function clear_event_cache( $pre, $timestamp, $hook, $args ) {
+	// Clear the specified timestamp
+	wp_cache_delete( event_cache_key( $hook, $args, $timestamp ), CACHE_GROUP );
+	// Clear the `null` timestamp too.
+	wp_cache_delete( event_cache_key( $hook, $args, null ), CACHE_GROUP );
+
+	return $pre;
+}
+
+function clear_event_cache_by_obj( $pre, $event ) {
+	return clear_event_cache( $event->timestamp, $event->hook, $event->args );
 }
 
 /**
